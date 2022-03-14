1f51624b2e9c2469df17714e508cf7564afbb274
shubug1015 <shubug@naver.com>
2022-03-14 22:25:38 +0900

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import { ApolloServer } from 'apollo-server';
+import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
+import { schema } from './schema';
+import { context } from './context';
+
+export const server = new ApolloServer({
+  schema,
+  context,
+  introspection: true,
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+});
+
+const port = process.env.PORT || 8000;
+
+server.listen({ port }).then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
